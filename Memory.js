a5f4f6d37eb6242f1ad939e7927a97eae6c0c1a6
nicolaecebotari <nicolaecebotari@alumnat.iesdamiahuguet.net>
2025-03-21 12:45:15 +0100

    inici

---
+++ Memory.js
@@ -0,0 +1,21 @@
+console.log('Happy developing ✨')
+posició = [1,2,3,4,5,6,7,8]
+colors = ['red','blue','yellow','green','purple','pink','orange','black']
+
+
+async function mostrar_colors(temps){
+    for (let i=0;i<colors.length;i++){
+        //TODO instruccions per mostrar el color. Per exemple
+
+        //Crida al mètode esperar
+        await esperar(temps);
+        console.log(colors[Math.floor(Math.random() * 9)]);
+    }
+}
+function esperar(milliseconds){
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+
+mostrar_colors(500);
